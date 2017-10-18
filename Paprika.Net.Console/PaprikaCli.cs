7e410834488ecef6d9bd0416526c6d7f7d1aa673
SteGriff <github@stegriff.co.uk>
2017-10-18 17:55:55 +0100

    Much improved CLI

---
+++ Paprika.Net.Console/PaprikaCli.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Con = System.Console;
+
+namespace Paprika.Net.Console
+{
+    public class PaprikaCli
+    {
+        private Core engine = new Core();
+        private RunModes mode = RunModes.ConfiguredManifest;
+        private string grammarSource = "";
+        private string prompt = "";
+
+        public void Run()
+        {
+            LoadConfigured();
+
+            Con.WriteLine("Welcome to Paprika. Type a phrase to resolve it, or type //? for extra commands.");
+
+            while (true)
+            {
+                //Display prompt and get input
+                Con.Write(prompt);
+                string input = Con.ReadLine();
+
+                //Skip back to input if nothing was entered
+                if (String.IsNullOrWhiteSpace(input))
+                {
+                    continue;
+                }
+
+                //Check for commands starting '//'
+                if (input.Length > 1 && input.Substring(0, 2) == "//")
+                {
+                    //Parameterise the commands after the slashes
+                    var commands = input
+                        .Replace("//", "")
+                        .Split(new[] { ' ' });
+
+                    switch (commands[0])
+                    {
+                        case "?":
+                            HelpText();
+                            continue;
+
+                        case "test":
+                            TestGrammar(engine);
+                            continue;
+
+                        case "reload":
+                            Con.WriteLine("Reloading...");
+                            Reload();
+                            Con.WriteLine("Done");
+                            continue;
+
+                        case "manifest":
+                            LoadFromCommand(commands);
+                            continue;
+                    }
+                }
+
+                //Do the actual parsing
+                try
+                {
+                    string output = engine.Parse(input);
+                    Con.WriteLine(output);
+                }
+                catch (BracketResolutionException ex)
+                {
+                    Con.WriteLine("{0} in [{1}]", ex.Message, ex.Category);
+                }
+                catch (FormatException ex)
+                {
+                    Con.WriteLine(ex.Message);
+                }
+                catch (InputException ex)
+                {
+                    Con.WriteLine(ex.Message);
+                }
+
+            }
+        }
+
+        private void TestGrammar(Core engine)
+        {
+            var grammar = engine.Grammar;
+            foreach (var g in grammar)
+            {
+                Con.WriteLine(g.Key);
+                foreach (var x in g.Value)
+                {
+                    Con.WriteLine("\t{0}", x);
+                }
+            }
+            Con.WriteLine("Press Return");
+            Con.ReadLine();
+        }
+
+        private void LoadConfigured()
+        {
+            mode = RunModes.ConfiguredManifest;
+            prompt = ">";
+
+            Reload();
+        }
+
+        private void LoadFromCommand(string[] commands)
+        {
+            if (commands.Length > 1)
+            {
+                string target = commands[1];
+                LoadSpecific(target);
+            }
+            else
+            {
+                Con.WriteLine("You didn't specify a file after 'manifest' command, so I'll reload the configured grammar.");
+                LoadConfigured();
+            }
+        }
+
+        private void LoadSpecific(string grammar)
+        {
+            mode = RunModes.SpecifiedManifest;
+            grammarSource = grammar;
+            prompt = grammar.Split(Path.DirectorySeparatorChar).Last() + " >";
+
+            Reload();
+        }
+
+        private void Reload()
+        {
+            engine = new Core();
+
+            try
+            {
+                switch (mode)
+                {
+                    case RunModes.ConfiguredManifest:
+                        engine.LoadConfiguredManifest();
+                        break;
+
+                    case RunModes.SpecifiedManifest:
+                        engine.LoadManifest(grammarSource);
+                        break;
+                }
+            }
+            catch (GrammarLoadingException ex)
+            {
+                Con.WriteLine(ex.Message);
+            }
+
+        }
+
+        private void HelpText()
+        {
+            Con.WriteLine("//? - This message");
+            Con.WriteLine("//reload - Reloads grammars from file");
+            Con.WriteLine("//test - Writes all grammar definitions to screen");
+            Con.WriteLine("//manifest [file] - Load a manifest file (a list of grammars)");
+        }
+    }
+}
--- Paprika.Net.Console/Program.cs
+++ Paprika.Net.Console/Program.cs
@@ -1,139 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Con = System.Console;
 
 namespace Paprika.Net.Console
 {
     class Program
     {
-        static Core engine = new Core();
-        static RunModes mode = RunModes.ConfiguredManifest;
-        static string grammarSource = "";
-        static string prompt = "";
-
         static void Main(string[] args)
         {
-            LoadConfigured();
-
-            while (true)
-            {
-                //Display prompt and get input
-                Con.Write( prompt );
-                string input = Con.ReadLine();
-
-                //Skip back to input if nothing was entered
-                if (String.IsNullOrWhiteSpace(input))
-                {
-                    continue;
-                }
-
-                //Check for commands starting '//'
-                if (input.Length > 1 && input.Substring(0, 2) == "//")
-                {
-                    //Parameterise the commands after the slashes
-                    var commands = input
-                        .Replace("//", "")
-                        .Split(new[] { ' ' });
-
-                    switch (commands[0])
-                    {
-                        case "test":
-                            TestGrammar(engine);
-                            continue;
-
-                        case "reload":
-                            Con.WriteLine("Reloading...");
-                            Reload();
-                            Con.WriteLine("Done");
-                            continue;
-
-                        case "manifest":
-                            string target = commands[1];
-                            LoadSpecific(target);
-                            continue;
-                    }
-                }
-
-                //Do the actual parsing
-                try
-                {
-                    string output = engine.Parse(input);
-                    Con.WriteLine(output);
-                }
-                catch (BracketResolutionException ex)
-                {
-                    Con.WriteLine("{0} in [{1}]", ex.Message, ex.Category);
-                }
-                catch (FormatException ex)
-                {
-                    Con.WriteLine(ex.Message);
-                }
-                catch (InputException ex)
-                {
-                    Con.WriteLine(ex.Message);
-                }
-
-            }
-
-        }
-
-        static void TestGrammar(Core engine)
-        {
-            var grammar = engine.Grammar;
-            foreach (var g in grammar)
-            {
-                Con.WriteLine(g.Key);
-                foreach (var x in g.Value)
-                {
-                    Con.WriteLine("\t{0}", x);
-                }
-            }
-            Con.ReadLine();
-        }
-
-        static void LoadConfigured()
-        {
-            mode = RunModes.ConfiguredManifest;
-            prompt = ">";
-
-            Reload();
-        }
-
-        static void LoadSpecific(string grammar)
-        {
-            mode = RunModes.SpecifiedManifest;
-            grammarSource = grammar;
-            prompt = grammar.Split(Path.DirectorySeparatorChar).Last() + " >";
-
-            Reload();
-        }
-
-        static void Reload()
-        {
-            engine = new Core();
-
             try
             {
-                switch (mode)
-                {
-                    case RunModes.ConfiguredManifest:
-                        engine.LoadConfiguredManifest();
-                        break;
-
-                    case RunModes.SpecifiedManifest:
-                        engine.LoadManifest(grammarSource);
-                        break;
-                }
+                var cli = new PaprikaCli();
+                cli.Run();
             }
-            catch (GrammarLoadingException ex)
+            catch (Exception ex)
             {
-                Con.WriteLine(ex.Message);
+                Con.WriteLine("Oops - there was a fatal exception and Paprika has to close:");
+                Con.WriteLine(ex.ToString());
             }
-
         }
+
     }
 }
